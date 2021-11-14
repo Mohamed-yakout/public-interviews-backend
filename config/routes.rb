8a2e23a9594a4e8cf634e328cd1040a4b421862e
Mohamed Yakout <mohamed.yakout91@gmail.com>
2021-11-14 19:14:11 +0400

    update seeds and routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,17 +5,12 @@ Rails.application.routes.draw do
       sessions: 'accounts/sessions'
   }
 
-
-  # devise_for :accounts
-
-  # devise_scope :account do
-  #   post 'sign_in', controller: "/accounts/sessions", action: :create
-  # end
-
-  resources :transactions, only: [:index, :create, :show]
-
-  # post 'sign_in', to: 'sessions#create', as: 'log_in'
-  # delete 'logout', to: 'sessions#destroy'
+  resources :transactions, only: [:index, :create, :show] do
+    collection do
+      get 'sent_transactions'
+      get 'received_transactions'
+    end
+  end
 
   root 'transactions#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
